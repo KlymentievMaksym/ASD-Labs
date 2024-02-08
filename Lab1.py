e4b31d0f796db9b708b6bc3b97e9d492ad03ed64
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-02-08 22:55:44 +0200

    1, 3 and 5 almost ready

--- Lab1.py
+++ Lab1.py
@@ -34,29 +34,35 @@ class towers:
         print(self._first_stick, self._second_stick, self._third_stick, '\n')
     
     def from1to3(self):
+        print("Used: 13")
         self.visualise(self._third_stick, self._first_stick)
     
     def from1to3to2from3to2(self):
+        print("Used: 13232")
         self.visualise(self._third_stick, self._first_stick)#
         self.visualise(self._second_stick, self._first_stick)#
         self.visualise(self._second_stick, self._third_stick)#
         
     def from1to3from2to1to3(self):
+        print("Used: 13213")
         self.visualise(self._third_stick, self._first_stick)##
         self.visualise(self._first_stick, self._second_stick)##
         self.visualise(self._third_stick, self._second_stick)##
         
     def from3to1to2from2to1(self):
+        print("Used: 31221")
         self.visualise(self._first_stick, self._third_stick)###
         self.visualise(self._first_stick, self._second_stick)###
         self.visualise(self._second_stick, self._third_stick)###
         
     def from1to3from2to1from3to2(self):
+        print("Used: 132132")
         self.visualise(self._third_stick, self._first_stick)####
         self.visualise(self._first_stick, self._second_stick)####
         self.visualise(self._second_stick, self._third_stick)####
         
     def from3to1from2to1to3(self):
+        print("Used: 31213")
         self.visualise(self._first_stick, self._third_stick)#####
         self.visualise(self._first_stick, self._second_stick)#####
         self.visualise(self._third_stick, self._second_stick)#####
@@ -66,36 +72,17 @@ class towers:
         if self._turn == 1:
             print(self._first_stick, self._second_stick, self._third_stick, '\n')
         if self._disk_count % 2 != 0:
-            if self._disk_count == 1:
+            if len(self._third_stick) == self._disk_count-1: #and "=" not in self._first_stick
                 self.from1to3()
-            elif self._disk_count == 3:
+            elif self._turn == 1 or self._turn == 3 or self._turn == 5 or self._turn == 6:
                 self.from1to3to2from3to2()
-                
                 self.from1to3from2to1to3()
-                
-                self.from1to3()
-            elif self._disk_count == 5:
+            elif self._turn == 2:
                 self.from1to3to2from3to2()
-                
-                self.from1to3from2to1to3()
-                
-                self.from1to3to2from3to2()
-                
                 self.from3to1to2from2to1()
-                
-                self.from1to3to2from3to2()
-                
-                self.from1to3from2to1to3()
-                
+            elif self._turn == 4:
                 self.from1to3from2to1from3to2()
-                
                 self.from3to1from2to1to3()
-                
-                self.from1to3to2from3to2()
-                
-                self.from1to3from2to1to3()
-                
-                self.from1to3()
         else:
             # self.visualise(self._, self._)
             if self._disk_count == 2:
