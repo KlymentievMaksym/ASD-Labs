98517b401c7ab280cf1276e35da8e3358341edc0
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-02-08 18:45:13 +0200

    Task10 is Done

--- Lab1.py
+++ Lab1.py
@@ -1,27 +1,18 @@
 # 10. Знайти найменше спільне кратне (НСК) двох цілих чисел за
 # допомогою рекурсії.
+
+def gcd(num1, num2):
+    if num2 == 0:
+        return num1
+    return gcd(num2, num1%num2)
+
+
+def lcm(num1, num2):
+    return num1*num2/gcd(num1, num2)
+
+
+print(lcm(3, 7))
+
 # 14. Реалізувати алгоритм для розв’язання задачі «Ханойські вежі».
 # Виписати послідовність ходів для перекладання n дисків вежі (n = 2;
 # 3; 4; 5 дисків, використати онлайн гру).
-
-import numpy as np
-
-
-
-
-def lcd(num1, num2, dict_for_prime_num1={}, dict_for_prime_num2={}):
-    def find_one_prime_number(num, dict_for_prime_num={}):
-        if num != 1:
-            for i in range(np.inf):
-                if num%i == 0:
-                    return i
-    prime_num1 = find_one_prime_number(num1, dict_for_prime_num1={})
-    
-
-# if num1 == 1 and num2 == 1:
-#     return 1
-# for prime in dict_for_prime_num1:
-#     num1 = num1/prime
-# for prime in dict_for_prime_num2:
-#     num2 = num2/prime
-# return 
