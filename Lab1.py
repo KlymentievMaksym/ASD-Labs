4cfe88ef906b06c23f9fd6f96cf2643f4a2419cc
Dadmon <maksklym-ipt27@lll.kpi.ua>
2024-02-09 20:48:12 +0200

    Welp, It Works

--- Lab1.py
+++ Lab1.py
@@ -22,96 +22,33 @@ class towers:
         self._disk_count = disk_count
         self._first_stick = ['='*i for i in range(1, disk_count+1)]
         self._first_stick.reverse()
+        self._needed_form = self._first_stick.copy()
         self._second_stick = []
         self._third_stick = []
+        self.dict = {0:self._disk_count, 1:self._first_stick, 2:self._second_stick, 3:self._third_stick}
         self._turn = 1
-        self._mini_turn = 1
         
     def visualise(self, stick1, stick2):
-        print(f"Current miniturn is: {self._mini_turn}") #\nFrom {stick2} to {stick1}S
-        stick1 += [stick2.pop(-1)]
-        self._mini_turn += 1
-        print(self._first_stick, self._second_stick, self._third_stick, '\n')
-    
-    def from1to3(self):
-        print("Used: 13")
-        self.visualise(self._third_stick, self._first_stick)
-    
-    def from1to3to2from3to2(self):
-        print("Used: 13232")
-        self.visualise(self._third_stick, self._first_stick)#
-        self.visualise(self._second_stick, self._first_stick)#
-        self.visualise(self._second_stick, self._third_stick)#
-        
-    def from1to3from2to1to3(self):
-        print("Used: 13213")
-        self.visualise(self._third_stick, self._first_stick)##
-        self.visualise(self._first_stick, self._second_stick)##
-        self.visualise(self._third_stick, self._second_stick)##
-        
-    def from3to1to2from2to1(self):
-        print("Used: 31221")
-        self.visualise(self._first_stick, self._third_stick)###
-        self.visualise(self._first_stick, self._second_stick)###
-        self.visualise(self._second_stick, self._third_stick)###
-        
-    def from1to3from2to1from3to2(self):
-        print("Used: 132132")
-        self.visualise(self._third_stick, self._first_stick)####
-        self.visualise(self._first_stick, self._second_stick)####
-        self.visualise(self._second_stick, self._third_stick)####
-        
-    def from3to1from2to1to3(self):
-        print("Used: 31213")
-        self.visualise(self._first_stick, self._third_stick)#####
-        self.visualise(self._first_stick, self._second_stick)#####
-        self.visualise(self._third_stick, self._second_stick)#####
-        
-    def start_turn(self):
-        print(f"Current turn is: {self._turn}")
-        if self._turn == 1:
-            print(self._first_stick, self._second_stick, self._third_stick, '\n')
-        if self._disk_count % 2 != 0:
-            if len(self._third_stick) == self._disk_count-1: #and "=" not in self._first_stick
-                self.from1to3()
-            elif self._turn == 1 or self._turn == 3 or self._turn == 5 or self._turn == 6:
-                self.from1to3to2from3to2()
-                self.from1to3from2to1to3()
-            elif self._turn == 2:
-                self.from1to3to2from3to2()
-                self.from3to1to2from2to1()
-            elif self._turn == 4:
-                self.from1to3from2to1from3to2()
-                self.from3to1from2to1to3()
-        else:
-            # self.visualise(self._, self._)
-            if self._disk_count == 2:
-                self.visualise(self._second_stick, self._first_stick)
-                self.visualise(self._third_stick, self._first_stick)
-                self.visualise(self._third_stick, self._second_stick)
-            elif self._disk_count == 4:
-                self.visualise(self._second_stick, self._first_stick)
-                self.visualise(self._third_stick, self._first_stick)
-                self.visualise(self._third_stick, self._second_stick)
-                
-                self.visualise(self._second_stick, self._first_stick)
-                self.visualise(self._first_stick, self._third_stick)
-                self.visualise(self._second_stick, self._third_stick)
-                
-                self.visualise(self._second_stick, self._first_stick)
-                self.visualise(self._third_stick, self._first_stick)
-                self.visualise(self._third_stick, self._second_stick)
-                self.visualise(self._first_stick, self._second_stick)
-                self.visualise(self._first_stick, self._third_stick)
-                self.visualise(self._third_stick, self._second_stick)
-                
-                self.visualise(self._second_stick, self._first_stick)
-                self.visualise(self._third_stick, self._first_stick)
-                self.visualise(self._third_stick, self._second_stick)
+        print(f"""Current turn is: {self._turn}
+From {stick1}({list(self.dict.keys())[list(self.dict.values()).index(stick1)]})
+To {stick2}({[list(self.dict.keys())[list(self.dict.values()).index(stick2)], "2|3"][self._turn == 1]})
+""")
+        stick2 += [stick1.pop(-1)]
         self._turn += 1
-        if len(self._third_stick) != self._disk_count:
-            self.start_turn()
-            
-# start_towers(4)
+        print("Result: \n", self._first_stick, ' ', self._second_stick, ' ', self._third_stick, '\n', sep="")
+        
+ 
 
-towers(5).start_turn()
+def hanoi_tower(game_info, n, source, destination, auxiliary):
+    if n==1:
+        # print("Move disk 1 from source",source,"to destination",destination)
+        game_info.visualise(source, destination)
+        return
+    hanoi_tower(game_info, n-1, source, auxiliary, destination)
+    # print ("Move disk",n,"from source",source,"to destination",destination)
+    game_info.visualise(source, destination)
+    hanoi_tower(game_info, n-1, auxiliary, destination, source)
+
+n = int(input("Enter n for HannoiTower: "))
+game_info = towers(n)#.hanoi_tower()
+hanoi_tower(game_info, game_info._disk_count, game_info._first_stick, game_info._third_stick, game_info._second_stick)
